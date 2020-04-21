e5989d80010a9ec47bf50dd4a83903631347bf9c
Wilson Nicholas <wilzznic05@gmail.com>
2020-04-21 11:10:13 +0700

    Updated app theme to NoActionBar

---
+++ app/src/main/java/com/example/edamamapp/RecipeDetailsActivity.kt
@@ -0,0 +1,14 @@
+package com.example.edamamapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecipeDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recipe_details)
+
+
+    }
+}
